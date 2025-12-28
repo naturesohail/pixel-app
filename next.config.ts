8c729ebe0496677faa0c3df343ae6305d0b4d573
Sohail <mohammadsohail55362@gmail.com>
2025-12-28 22:26:07 +0530

    first push

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
- 
-       turbopack: {
-                 root: __dirname
-       },
-        images: {
-            remotePatterns: [
-                        {
-                         protocol: "https",
-                         hostname: "aioftheworld.com",
-                         pathname: "/**"
-                        }
-                          ] 
-       },
-    reactStrictMode: true,
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: `${process.env.BASE_URL||"*"}` },
-            { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
-            { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-          ],
-        },
-      ];
-    },
-  
-  
-};
+import type { NextConfig } from 'next'
 
-module.exports = nextConfig;
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  }
+}
+
+export default nextConfig
